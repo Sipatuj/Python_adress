2daf075d154c09ac7e75ce38ba45eebb7adb40d3
Konstantin <Sipatuj@gmail.com>
2014-09-09 18:46:33 +0300

    Update Pythons.py
    Traceback (most recent call last):
      File "/home/sipatuj/Komodo-Edit-8/ccc.py", line 15, in <module>
        Kniga().adds()
    TypeError: adds() takes exactly 4 arguments (1 given)

--- Pythons.py
+++ Pythons.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 class Kniga:
     def adds(self, name, phone, adress):
-        pass
+        self.name = name
+        self.name = phone
+        self.adress = adress
+        name = input('add name: ')
+        phone = input('add phone: ')
+        adress = input('add adress: ')
     def AdresKniga():
         bd={
             'name' : ('name','phone', 'adress'),
         }
-    
-    
+Kniga().adds()
