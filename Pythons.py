1e8a6e84078baf723f96e677c577b7adeabc4453
Konstantin <Sipatuj@gmail.com>
2014-09-11 23:54:25 +0300

    Update

--- Pythons.py
+++ Pythons.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-class Kniga:
-    def adds(self, name, phone, adress):
-        self.name = name
-        self.name = phone
-        self.adress = adress
-        name = input('add name: ')
-        phone = input('add phone: ')
-        adress = input('add adress: ')
-    def AdresKniga():
-        bd={
-            'name' : ('name','phone', 'adress'),
-        }
-Kniga().adds()
+import pickle
+
+file = 'test.txt'
+
+ab={}
+name = input('Vedite name: ')
+adress = input('Vedite adress: ')
+phone = input('Vedite phone: ')
+email = input('Vedite email: ')
+
+ab[name]=(name, adress,phone,email)
+
+f = open(file, 'wb')
+pickle.dump(ab, f)
+f.close
+
+del ab
+
+f = open(file, 'rb')
+loadlist = pickle.load(f)
+print(loadlist)
+
