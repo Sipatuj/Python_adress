9334dd61c84bb83da3d001c8a041f03df6dd6f45
Konstantin <Sipatuj@gmail.com>
2014-09-12 11:16:04 +0300

    Update

--- Pythons.py
+++ Pythons.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
-import pickle
 
-file = 'test.txt'
 
-ab={}
 name = input('Vedite name: ')
 adress = input('Vedite adress: ')
 phone = input('Vedite phone: ')
 email = input('Vedite email: ')
 
-ab[name]=(name, adress,phone,email)
-
-f = open(file, 'wb')
-pickle.dump(ab, f)
-f.close
-
-del ab
-
-f = open(file, 'rb')
-loadlist = pickle.load(f)
-print(loadlist)
+name=dict(name=name,adress=adress,phone=phone,email=email)
+ad=[]
+ad.append([name])
 
